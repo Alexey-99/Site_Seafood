7062bab9ab269cf99713a11ecaf3688440c95406
Alexey <koroza.alexey99@gmail.com>
2022-02-26 23:33:48 +0300

    *

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 $(function () {
   $('.header_slider').slick({
     dots: true,
-    prevArrow: '<button type="button" class="slick-prev">Previous</button>',
-    nextArrow: '<button type="button" class="slick-next">Next</button>'
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="images/prev.svg" alt="prev.svg"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="images/next.svg" alt="next.svg"></button>',
   });
 });
