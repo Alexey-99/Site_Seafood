9e336ff9c1b1aea8f4b0dd0762de61beed9d78a9
Alexey <koroza.alexey99@gmail.com>
2022-02-26 23:19:57 +0300

    *

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
-$(function(){
-
-    $('.header_slider').slick({
-      dots: true  
-    });
-
-
+$(function () {
+  $('.header_slider').slick({
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+    nextArrow: '<button type="button" class="slick-next">Next</button>'
+  });
 });
