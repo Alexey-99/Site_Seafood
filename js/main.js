fd4f829605fe9b2f8749ef2c260ade00829ba3cb
Alexey <koroza.alexey99@gmail.com>
2022-03-05 19:47:11 +0300

    *

--- js/main.js
+++ js/main.js
@@ -5,5 +5,6 @@ $(function () {
       '<button type="button" class="slick-prev"><img src="images/prev.svg" alt="prev.svg"></button>',
     nextArrow:
       '<button type="button" class="slick-next"><img src="images/next.svg" alt="next.svg"></button>',
+    fade: true
   });
 });
